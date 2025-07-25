35c09c4a4db6da5ede9b70581bb7e0fae384d1c3
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-25 22:28:48 +0200

    feat: complete exercise two

--- 01/exercises.js
+++ 01/exercises.js
@@ -6,3 +6,15 @@
 const fruits = ['cocomero', 'pera', 'mela', 'mango'];
 console.log(fruits.length);
 
+//  2. OGGETTI – Crea e accedi a un oggetto persona
+// Crea un oggetto chiamato person con tre proprietà: name, age, job
+// Stampa solo il nome della persona
+
+const person = {
+    name: 'Ambra',
+    age: 30,
+    job: 'designer'
+}
+
+console.log(`Nome: ${person.name}`);
+
