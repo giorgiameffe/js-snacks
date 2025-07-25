b2091387d503bf5eb526ddec0bf77ea80e6f95a0
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-25 22:26:36 +0200

    feat: complete exercise one

---
+++ 01/exercises.js
@@ -0,0 +1,8 @@
+// 1. ARRAY – Trova la lunghezza
+// Dato l’array:
+// const fruits = ["cocomero", "pera", "mela", "mango"];
+// Stampa il numero totale di elementi contenuti nell’array.
+
+const fruits = ['cocomero', 'pera', 'mela', 'mango'];
+console.log(fruits.length);
+
