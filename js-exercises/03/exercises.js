82af80e86003b5d216c335b45ca8833464ced73e
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-11 22:45:24 +0200

    feat: complete exercise six

--- js-exercises/03/exercises.js
+++ js-exercises/03/exercises.js
@@ -1,6 +1,4 @@
-// ====================
 // Esercizio 1: Filtrare numeri pari
-// ====================
 // Consegna: Dato un array di numeri, crea un nuovo array che contenga solo i numeri pari usando .filter()
 
 const numbers = [12, 3, 28, 6, 8, 17];
@@ -9,9 +7,7 @@ const evenNumbers = numbers.filter(number => number % 2 === 0);
 console.log(evenNumbers);
 
 
-// ====================
 // Esercizio 2: Sommare tutti i numeri
-// ====================
 // Consegna: Usando .reduce(), calcola la somma di tutti i numeri dell'array.
 
 const sumNumbers = numbers.reduce((acc, currentValue) => {
@@ -22,9 +18,7 @@ const sumNumbers = numbers.reduce((acc, currentValue) => {
 console.log(sumNumbers);
 
 
-// ====================
 // Esercizio 3: Filtrare prodotti costosi
-// ====================
 // Consegna: Dato un array di oggetti prodotto con proprietà nome e prezzo,
 // restituisci solo i prodotti con prezzo superiore a 50.
 
@@ -47,18 +41,14 @@ const filteredProducts = products.filter(product => product.price > 50);
 console.log(filteredProducts);
 
 
-// ====================
 // Esercizio 4: Ottenere solo i nomi dei prodotti
-// ====================
 // Consegna: Usando .map(), crea un array contenente solo i nomi dei prodotti.
 
 const productsName = products.map(product => product.name);
 console.log(productsName);
 
 
-// ====================
 // Esercizio 5: Calcolare il prezzo totale
-// ====================
 // Consegna: Usando .reduce(), calcola la somma dei prezzi di tutti i prodotti.
 
 const totalPrice = products.reduce((acc, product) => {
@@ -67,3 +57,14 @@ const totalPrice = products.reduce((acc, product) => {
 }, 0)
 
 console.log(totalPrice);
+
+
+// Esercizio 6: Trovare un elemento
+// Consegna: Usa .find() per ottenere il primo numero maggiore di 20 nell'array.
+
+{
+    const numbers = [24, 8, 17, 25];
+
+    const foundNumber = numbers.find(number => number > 20);
+    console.log(foundNumber);
+}
