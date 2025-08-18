40d390b0d2e93559b48dd654d16c6696bc32ade8
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-18 22:33:58 +0200

    feat: scaffolding

---
+++ js-exercises/05/exercises.js
@@ -0,0 +1,34 @@
+// ============================
+// 🌀 CICLO FOR
+// ============================
+
+
+// 1. Stampa i numeri da 0 a 4
+// CONSEGNA: Scrivi un ciclo for che stampi i numeri da 0 a 4.
+
+
+// 2. Fai un countdown da 5 a 1
+// CONSEGNA: Scrivi un ciclo for che conti da 5 fino a 1.
+
+
+// 3. Stampa solo i numeri pari da 0 a 10
+// CONSEGNA: Scrivi un ciclo for che stampi solo i numeri pari da 0 a 10.
+
+
+// 4. Calcola la somma dei numeri da 1 a 5
+// CONSEGNA: Scrivi un ciclo for che calcoli la somma dei numeri da 1 a 5
+// e alla fine stampi il risultato.
+
+
+// 5. Stampa tutti gli elementi di un array
+// CONSEGNA: Dato l’array ["🍎", "🍌", "🍇", "🍑"], stampane gli elementi uno per uno.
+
+
+// 6. Interrompi il ciclo con break
+// CONSEGNA: Scrivi un ciclo for che conta da 1 a 10 ma si ferma quando arriva a 5.
+
+
+// 7. Trova il quadrato dei primi 10 numeri
+// CONSEGNA: Scrivi un ciclo for che stampi i quadrati dei numeri da 1 a 10.
+// (es: 1, 4, 9, 16, ...)
+
