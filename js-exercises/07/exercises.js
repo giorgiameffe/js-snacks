d9bdacdf1041747a5731909508bdf59e8dff6207
giorgiameffe <giorgiameffe03@gmail.com>
2025-09-08 15:22:02 +0200

    feat: create folder for other exercises

---
+++ js-exercises/07/exercises.js
@@ -0,0 +1,33 @@
+// 🔹 ARRAY DI PARTENZA
+
+const books = [
+    { title: "Harry Potter", pages: 320, read: true },
+    { title: "Il Signore degli Anelli", pages: 1178, read: false },
+    { title: "1984", pages: 328, read: true },
+    { title: "Il Piccolo Principe", pages: 96, read: false }
+];
+
+// 📝 ESERCIZI
+
+// 1. Lunghezza dell'array
+
+
+// 2️. Array con solo i titoli (map)
+
+
+// 3️. Libri con più di 300 pagine (filter)
+
+
+// 4. Trova il primo libro già letto (find)
+
+
+// 5️. Stampa tutti i titoli con forEach
+
+
+// 6️. Conta quanti libri non sono stati letti (filter + length)
+
+
+// 7️. Crea un array con solo il numero di pagine (map)
+
+
+// 8️. Verifica se esiste un libro con meno di 100 pagine (some)
