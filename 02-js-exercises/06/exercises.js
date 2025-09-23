30849b4aeb28eaa01240e704865bba03daaba096
giorgiameffe <giorgiameffe03@gmail.com>
2025-09-23 12:33:03 +0200

    feat: scaffolding

---
+++ 02-js-exercises/06/exercises.js
@@ -0,0 +1,51 @@
+// 🔹 ARRAY DI PARTENZA
+
+const travels = [
+    { id: 1, destinazione: "Parigi", durata: 5, prezzo: 750, compagnia: "Air France" },
+    { id: 2, destinazione: "Roma", durata: 3, prezzo: 500, compagnia: "Alitalia" },
+    { id: 3, destinazione: "Tokyo", durata: 10, prezzo: 1500, compagnia: "Japan Airlines" },
+    { id: 4, destinazione: "New York", durata: 7, prezzo: 1200, compagnia: "Delta" },
+    { id: 5, destinazione: "Sydney", durata: 12, prezzo: 1800, compagnia: "Qantas" },
+    { id: 6, destinazione: "Londra", durata: 4, prezzo: 700, compagnia: "British Airways" },
+];
+
+// ----------------------
+// CICLO FOR
+// ----------------------
+
+// 1. Usa un ciclo for per stampare tutte le destinazioni dei viaggi.
+
+// 2. Calcola la somma totale dei prezzi di tutti i viaggi usando un ciclo for.
+
+// ----------------------
+// METODI ARRAY
+// ----------------------
+
+// 3. Stampare il prezzo di ogni viaggio.
+
+// 4. Creare un array di tutte le destinazioni dei viaggi.
+
+// 5. Ottenere tutti i viaggi che costano meno di 1000 euro.
+
+// 6. Trovare il primo viaggio con durata maggiore di 10 giorni.
+
+// 7. Creare un array solo delle compagnie aeree dei viaggi.
+
+// 8. Stampare una frase per ogni viaggio:"Parigi dura 5 giorni e costa 750 euro."
+
+// ----------------------
+// FUNZIONI
+// ----------------------
+
+// 9. Scrivi una funzione che restituisce un nuovo array contenente solo le destinazioni dei viaggi
+// il cui prezzo è maggiore di 1000 euro.
+
+// 10. Scrivi una funzione che prende il nome di una destinazione e usa find per restituire l’oggetto viaggio corrispondente.
+
+// ----------------------
+// METODI COMBINATI
+// ----------------------
+
+// 11. Creare un array con i viaggi che durano più di 5 giorni e poi  ottenere solo le destinazioni.
+
+// 12. Conta quanti viaggi durano meno di 6 giorni usando filter e la proprietà length.
