8af7e64eb551af26c1974b7b18e814cacbf887cd
giorgiameffe <giorgiameffe03@gmail.com>
2025-09-29 16:35:02 +0200

    feat: scaffolding

---
+++ 03-js-exercises/02/exercises.js
@@ -0,0 +1,58 @@
+// 🔹 ARRAY DI PARTENZA
+
+const pets = [
+    { name: "Buddy", type: "Dog", age: 4, country: "USA", weight: 25 },
+    { name: "Milo", type: "Cat", age: 2, country: "UK", weight: 6 },
+    { name: "Charlie", type: "Dog", age: 7, country: "Canada", weight: 30 },
+    { name: "Luna", type: "Cat", age: 3, country: "Italy", weight: 5 },
+    { name: "Rocky", type: "Dog", age: 6, country: "USA", weight: 28 },
+    { name: "Bella", type: "Rabbit", age: 1, country: "France", weight: 2 },
+    { name: "Kiwi", type: "Parrot", age: 5, country: "Brazil", weight: 1 },
+    { name: "Max", type: "Dog", age: 8, country: "Germany", weight: 35 },
+    { name: "Oliver", type: "Cat", age: 4, country: "Spain", weight: 7 },
+    { name: "Coco", type: "Parrot", age: 2, country: "Mexico", weight: 1 }
+];
+
+
+// ----------------------
+// CICLO FOR
+// ----------------------
+
+// 1. Stampare tutti i nomi dei pets.
+// 2. Sommare tutte le età dei pets.
+// 3. Stampare i pets che pesano più di 10 kg.
+
+
+// ----------------------
+// METODI ARRAY
+// ----------------------
+
+// 4. Stampare tipo e paese di ogni pet.
+// 5. Creare un array con solo i nomi dei pets.
+// 6. Ottenere tutti i gatti (type === "Cat").
+// 7. Trovare il primo pet con meno di 2 anni.
+// 8. Creare frasi tipo: "[name] è un [type] proveniente da [country]".
+// 9. Ottenere solo i pets provenienti dagli USA.
+// 10. Trovare un pet specifico (ad esempio "Buddy").
+// 11. Stampare il nome e l’età di ogni pet.
+// 12. Creare un array con solo i pesi dei pets.
+// 13. Calcolare la somma totale dei pesi.
+
+
+// ----------------------
+// ACCESSO & LENGTH
+// ----------------------
+
+// 14. Stampare il nome del primo pet.
+// 15. Stampare il nome dell’ultimo pet.
+// 16. Stampare quanti pets ci sono nell’array.
+// 17. Stampare nome e tipo del penultimo pet.
+
+
+// ----------------------
+// METODI COMBINATI
+// ----------------------
+
+// 18. Creare un array con i nomi dei pets che pesano meno di 10 kg.
+// 19. Stampare i cani con frase tipo: "Il cane [name] ha [age] anni".
+// 20. Stampare i nomi dei pets con età >= 3 e peso <= 7 kg.
