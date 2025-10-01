5e71a30115c296f4dd6a31be6bd9a3fd160b1121
giorgiameffe <giorgiameffe03@gmail.com>
2025-10-01 11:51:21 +0200

    feat: complete 13 snacks

--- 03-js-exercises/04/exercises.js
+++ 03-js-exercises/04/exercises.js
@@ -17,10 +17,28 @@ const hobbies = [
 // ----------------------
 
 // 1. Quanti hobby ci sono nell’array?
+
+console.log('Numero hobby:', hobbies.length);
+
 // 2. Stampa il nome del primo hobby
+
+const firstHobby = hobbies[0];
+console.log(firstHobby.name);
+
 // 3. Stampa il tipo dell’ultimo hobby
+
+const lastHobby = hobbies[hobbies.length - 1];
+console.log(lastHobby.type);
+
 // 4. Stampa il nome del terzo hobby
+
+const thirdHobby = hobbies[2];
+console.log(thirdHobby.name);
+
 // 5. Stampa le ore settimanali del quarto hobby
+
+const fourthHobby = hobbies[3];
+console.log(fourthHobby.weeklyHours);
 
 
 // ----------------------
@@ -28,13 +46,42 @@ const hobbies = [
 // ----------------------
 
 // 6. Crea un array con solo i nomi degli hobby
+
+const hobbiesNames = hobbies.map(h => h.name);
+console.log(hobbiesNames);
+
 // 7. Crea un array con solo le ore settimanali
+
+const weeklyHours = hobbies.map(h => h.weeklyHours);
+console.log(weeklyHours);
+
 // 8. Filtra gli hobby con meno di 5 ore settimanali
+
+const shorHobbies = hobbies.filter(h => h.weeklyHours < 5);
+console.log(shorHobbies);
+
 // 9. Filtra gli hobby che si praticano indoor
+
+const indoorHobbies = hobbies.filter(h => h.indoor);
+console.log(indoorHobbies);
+
 // 10. Stampa una frase per ogni hobby del tipo: "X è un Y che richiede Z ore a settimana"
+
+hobbies.forEach(h => console.log(`${h.name} è un ${h.type} che richiede ${h.weeklyHours} a settimana`));
+
 // 11. Stampa indice e nome di ciascun hobby
+
+hobbies.forEach((h, i) => console.log(`Indice: ${i} - Nome: ${h.name}`));
+
 // 12. Trova il primo hobby outdoor
+
+const firstOutdoorHobby = hobbies.find(h => h.indoor === false);
+console.log(firstOutdoorHobby);
+
 // 13. Trova l’hobby che occupa esattamente 8 ore a settimana
+
+const eightHoursHobby = hobbies.find(h => h.weeklyHours === 8);
+console.log(eightHoursHobby);
 
 
 // ----------------------
